b9a2b7323a6582e11070aa4e7081bcb0a770be00
Carlos Ble <carlos@codesai.com>
2019-02-19 13:13:56 +0100

    delete confusing comment

--- src/main/java/implementationChange/ExternalCartClient.java
+++ src/main/java/implementationChange/ExternalCartClient.java
@@ -1,9 +1,5 @@
 package implementationChange;
 
-/*
-  Imagine this is an external client which code
-  you can't change.
- */
 public class ExternalCartClient {
     public String formattedTotalPrice(int price) {
         ShoppingCart shoppingCart = new ShoppingCart();
